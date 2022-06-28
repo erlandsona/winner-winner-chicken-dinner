e10ceda4870ea6c7380405a556a3167febbdbed9
Austin Erlandson <austinerlandson77@gmail.com>
2022-06-28 08:56:27 -0500

    Hook up Prisma

--- app/Route/Hello/Id_.elm
+++ app/Route/Hello/Id_.elm
@@ -1,4 +1,4 @@
-module Route.Hello exposing (ActionData, Data, Model, Msg, route)
+module Route.Hello.Id_ exposing (ActionData, Data, Model, Msg, route)
 
 import DataSource exposing (DataSource)
 import DataSource.Port as Port
@@ -28,7 +28,7 @@ type Msg
 
 
 type alias RouteParams =
-    {}
+    { id : String }
 
 
 route : StatefulRoute RouteParams Data ActionData Model Msg
@@ -73,6 +73,11 @@ subscriptions maybePageUrl routeParams path sharedModel model =
     Sub.none
 
 
+pages : DataSource (List RouteParams)
+pages =
+    DataSource.succeed []
+
+
 type alias Data =
     String
 
@@ -84,7 +89,7 @@ type alias ActionData =
 data : RouteParams -> Request.Parser (DataSource (Response Data ErrorPage))
 data routeParams =
     Request.succeed
-        (Port.get "hello" (Encode.string "Austin") Decode.string
+        (Port.get "hello" (Encode.string routeParams.id) Decode.string
             |> DataSource.map Response.render
         )
 
--- port-data-source.ts
+++ port-data-source.ts
@@ -1,3 +1,7 @@
-export async function hello(name) {
-  return `Hello ${name}!`;
+import { prisma } from './prisma/client'
+
+export async function hello(id: string) {
+  const maybeData = prisma.pollQuestion.findFirst({ where: { id: id } })
+  console.log(maybeData)
+  return JSON.stringify(maybeData)
 }
